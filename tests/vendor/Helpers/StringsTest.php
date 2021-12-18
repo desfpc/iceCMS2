2ddaaa6a5a04e3874de807facf0e0dfab9f9f9d3
Sergey Peshalov <desfpc@gmail.com>
2021-12-18 10:56:54 +0300

    Strings helpers class tests

---
+++ tests/vendor/Helpers/StringsTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+/**
+ * iceCMS2 v0.1a
+ * Created by Sergey Peshalov https://github.com/desfpc
+ * https://github.com/desfpc/iceCMS2
+ *
+ * HelpersStrings Class Tests
+ */
+
+namespace vendor\Helpers;
+
+use iceCMS2\Helpers\Strings;
+use PHPUnit\Framework\TestCase;
+
+class StringsTest extends TestCase
+{
+    /**
+     * Test iceCMS2\Helpers\Strings::camelToSnake
+     *
+     * @return void
+     */
+    public function testCamelToSnake(): void
+    {
+        $this->assertEquals([
+            'first_camel',
+            'second_camel',
+            'third_camel_xxl',
+        ], [
+            Strings::camelToSnake('firstCamel'),
+            Strings::camelToSnake('SecondCamel'),
+            Strings::camelToSnake('ThirdCamelXXL'),
+        ]);
+    }
+
+    /**
+     * Test iceCMS2\Helpers\Strings::snakeToCamel
+     *
+     * @return void
+     */
+    public function testSnakeToCamel(): void
+    {
+        $this->assertEquals([
+            'firstCamel',
+            'secondCamel',
+            'thirdCamelXxl',
+            'UpperCamel',
+        ], [
+            Strings::snakeToCamel('first_camel', true),
+            Strings::snakeToCamel('second_camel', true),
+            Strings::snakeToCamel('third_camel_xxl', true),
+            Strings::snakeToCamel('upperCamel', false),
+        ]);
+    }
+}
