efd66f3ca2935e7a54389dc9820858a83bd56ac9
Sergey Peshalov <desfpc@gmail.com>
2021-12-18 10:34:05 +0300

    Strings helpers class

---
+++ vendor/desfpc/icecms2/Helpers/Strings.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+/**
+ * iceCMS2 v0.1a
+ * Created by Sergey Peshalov https://github.com/desfpc
+ * https://github.com/desfpc/iceCMS2
+ *
+ * Strings Helpers
+ */
+
+namespace iceCMS2\Helpers;
+
+class Strings
+{
+    /**
+     * Convert CamelCase string to snake_case
+     *
+     * @param string $camel string in CamelCase or lowerCamelCase
+     * @return string string in snake_case
+     */
+    public static function camelToSnake(string $camel): string
+    {
+
+    }
+
+    /**
+     * @param string $snake string in snake_case
+     * @param bool $isLowerCamelCase loverCanelCase flag
+     * @return string string in CamelCase or lowerCamelCase
+     */
+    public static function snakeToCamel(string $snake, bool $isLowerCamelCase = true): string
+    {
+
+    }
+}
