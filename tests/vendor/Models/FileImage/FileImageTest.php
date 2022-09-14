5d424a730b437312361893b05646a5437aa0062d
Sergey Peshalov <desfpc@gmail.com>
2022-09-14 16:01:43 +0400

    FileImageSize test - in work

---
+++ tests/vendor/Models/FileImage/FileImageTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+/**
+ * iceCMS2 v0.1a
+ * Created by Sergey Peshalov https://github.com/desfpc
+ * https://github.com/desfpc/iceCMS2
+ *
+ * File Model Classes Tests
+ */
+
+namespace vendor\Models\FileImage;
+
+use iceCMS2\Models\ImageSize;
+use iceCMS2\Tools\Exception;
+use iceCMS2\Tests\Ice2CMSTestCase;
+
+class FileImageTest extends Ice2CMSTestCase
+{
+    /**
+     * DB Tables used for testing
+     */
+    protected static array $_dbTables = ['users', 'files', 'image_sizes', 'file_image_sizes'];
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct()
+    {
+        if (session_id() === ''){
+            session_start();
+        }
+        parent::__construct();
+    }
+
+    /**
+     * File Image all entity test
+     * @throws Exception
+     */
+    public function testFileImage(): void
+    {
+        //TODO Creating watermark image
+        $watermarkId = 1;
+
+        //TODO Creating test image sizes
+        $imageSizeArr = [
+            [
+                'width' => 0,
+                'height' => 300,
+                'string_id' => '0_300',
+            ],
+            [
+                'width' => 300,
+                'height' => 0,
+                'string_id' => '300_0',
+            ],
+            [
+                'width' => 300,
+                'height' => 300,
+                'string_id' => '300',
+            ],
+            [
+                'width' => 800,
+                'height' => 400,
+                'string_id' => '800_400_' . $watermarkId,
+                'watermark_id' => $watermarkId,
+                'watermark_width' => 50,
+                'watermark_height' => 60,
+                'watermark_top' => -10,
+                'watermark_left' => 10,
+                'watermark_units' => 'px',
+            ],
+        ];
+
+        foreach ($imageSizeArr as $item) {
+            $imageSize = new ImageSize(self::$_testSettings);
+            $imageSize->set($item);
+            $this->assertTrue($imageSize->save());
+        }
+
+        //TODO Creating test image
+
+        //TODO Adding imageSizes to Image
+
+        //TODO Deleting imageSizes
+
+        //TODO Deleting Image
+    }
+}
