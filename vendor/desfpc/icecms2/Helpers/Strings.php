19515f50f42e56075767ebac962f446122783d3f
Sergey Peshalov <desfpc@gmail.com>
2022-09-20 02:57:43 +0400

    Starting User entity, Strings helpers, updated users table

--- vendor/desfpc/icecms2/Helpers/Strings.php
+++ vendor/desfpc/icecms2/Helpers/Strings.php
@@ -15,6 +15,128 @@ use iceCMS2\Settings\Settings;
 class Strings
 {
     /**
+     * Generate random string
+     *
+     * @param int $len
+     * @param string|null $alphabet
+     * @return string
+     */
+    public static function getRandomString(int $len = 8, ?string $alphabet = null): string
+    {
+        if (is_null($alphabet)) {
+            $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+=?';
+        }
+        $pass = [];
+        $alphaLength = strlen($alphabet) - 1;
+        for ($i = 0; $i < $len; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass);
+    }
+
+    /**
+     * Generate random password
+     *
+     * @param int $minLen
+     * @return string
+     */
+    public function getRandomPassword(int $minLen = 8): string
+    {
+        $pas = self::getRandomString($minLen);
+        $errorCode = self::getPassError($pas);
+
+        if ($errorCode === 2) {
+            $pas = $pas . self::getRandomString(1, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+            $errorCode = self::getPassError($pas);
+        }
+        if ($errorCode === 3) {
+            $pas = $pas . self::getRandomString(1, 'abcdefghijklmnopqrstuvwxyz');
+            $errorCode = self::getPassError($pas);
+        }
+        if ($errorCode === 4) {
+            $pas = $pas . self::getRandomString(1, '1234567890');
+        }
+
+        return $pas;
+    }
+
+    /**
+     * Check password is secure and get error code (0 - if no error and password is secure)
+     *
+     * @param string $text
+     * @return int
+     */
+    public static function getPassError(string $text): int
+    {
+        if (mb_strlen($text, 'UTF-8') < 8) {
+            return 1;
+        }
+        if (!preg_match('@[A-ZА-ЯЁ]@', $text)) {
+            return 2;
+        }
+        if (!preg_match('@[a-zа-яё]@', $text)) {
+            return 3;
+        }
+        if (!preg_match('@[0-9]@', $text)) {
+            return 4;
+        }
+        return 0;
+    }
+
+    /**
+     * Text transliterate
+     *
+     * @param string $string
+     * @param string $language
+     * @return string
+     */
+    public static function transliterate(string $string, string $language): string
+    {
+        return match ($language) {
+            'Ru' => self::transliterateRu($string),
+            default => $string,
+        };
+    }
+
+    /**
+     * Text transliterate from Ru to En
+     *
+     * @param string $string
+     * @return string
+     */
+    public static function transliterateRu(string $string): string
+    {
+        $table = array(
+            'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D',
+            'Е' => 'E', 'Ё' => 'YO', 'Ж' => 'ZH', 'З' => 'Z', 'И' => 'I',
+            'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N',
+            'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T',
+            'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C', 'Ч' => 'CH',
+            'Ш' => 'SH', 'Щ' => 'SCH', 'Ь' => '', 'Ы' => 'Y', 'Ъ' => '',
+            'Э' => 'E', 'Ю' => 'YU', 'Я' => 'YA',
+
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
+            'е' => 'e', 'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i',
+            'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
+            'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+            'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+            'ш' => 'sh', 'щ' => 'sch', 'ь' => '', 'ы' => 'y', 'ъ' => '',
+            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+        );
+
+        $output = str_replace(
+            array_keys($table),
+            array_values($table), $string
+        );
+
+        $output = preg_replace('/[^-a-z0-9._\[\]\'"]/i', ' ', $output);
+        $output = preg_replace('/ +/', '-', $output);
+
+        return $output;
+    }
+
+    /**
      * Convert CamelCase string to snake_case
      *
      * @param string $camel string in CamelCase or lowerCamelCase
---
+++ vendor/desfpc/icecms2/Models/User.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+/**
+ * iceCMS2 v0.1a
+ * Created by Sergey Peshalov https://github.com/desfpc
+ * https://github.com/desfpc/iceCMS2
+ *
+ * User entity class
+ */
+
+namespace iceCMS2\Models;
+
+use iceCMS2\Helpers\Strings;
+use iceCMS2\Tools\Exception;
+
+class User extends AbstractEntity
+{
+    /** @var string Entity DB table name */
+    protected string $_dbtable = 'users';
+
+    /** @var array Array of needed to approve values */
+    protected array $_needToApproveValues = ['phone', 'email'];
+
+    /**
+     * TODO Logics after Entity load() method
+     *
+     * @return void
+     */
+    protected function _afterLoad(): void
+    {
+
+    }
+
+    /**
+     * Check approved code and update approved status
+     *
+     * @param string $codeType
+     * @param string $inputValue
+     * @return bool
+     * @throws Exception
+     */
+    public function checkApproveCode(string $codeType, string $inputValue): bool
+    {
+        if ($this->get($codeType . '_approve_code') === $inputValue) {
+            $this->set($codeType. '_approved', 1);
+            return $this->save();
+        }
+
+        return false;
+    }
+
+    /**
+     * Send approve code to user
+     *
+     * @param string $codeType
+     * @return bool
+     * @throws Exception
+     */
+    public function sendApproveCode(string $codeType): bool
+    {
+        $code = $this->_getApproveCode();
+        $this->set($codeType. '_approve_code', $code);
+        $this->set($codeType. '_approved', 0);
+        $this->set($codeType. '_send_time');
+        if ($this->save() && $this->_sendApproveCodeMessage($codeType)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * TODO send approve code message
+     *
+     * @param string $codeType
+     * @return bool
+     */
+    private function _sendApproveCodeMessage(string $codeType): bool
+    {
+        switch ($codeType) {
+            case 'phone':
+                break;
+            case 'email':
+                break;
+        }
+
+        return false;
+    }
+
+    /**
+     * Generate and get approve code string
+     *
+     * @return string
+     */
+    private function _getApproveCode():string
+    {
+        return Strings::getRandomString(6, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890');
+    }
+}
