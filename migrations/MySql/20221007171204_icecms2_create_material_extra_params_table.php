7b503251988fbaaef8482bea01bd34edc2233be0
Sergey Peshalov <desfpc@gmail.com>
2022-10-08 00:04:25 +0400

    Update 20221007171204_icecms2_create_material_extra_params_table.php

--- migrations/MySql/20221007171204_icecms2_create_material_extra_params_table.php
+++ migrations/MySql/20221007171204_icecms2_create_material_extra_params_table.php
@@ -21,7 +21,17 @@ class Icecms2CreateMaterialExtraParamsTable extends AbstractMigration
      */
     public function up(): string
     {
-        return '';
+        return "
+        CREATE TABLE `material_extra_params`  (
+            `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+            `name` varchar(255) NOT NULL COMMENT 'Param name',
+            `mtype_id` int(11) NOT NULL COMMENT 'Material type ID',
+            `value_type` ENUM('int', 'bool', 'varchar', 'text', 'float', 'decimal') NOT NULL COMMENT 'Param value type',
+            `value_mtype` int UNSIGNED NULL DEFAULT NULL COMMENT 'Material type ID',
+            PRIMARY KEY (`id`) USING BTREE,
+            INDEX `mep_mtype_idx`(`mtype_id`) USING BTREE
+        ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+        ";
     }
 
     /**
@@ -31,6 +41,6 @@ class Icecms2CreateMaterialExtraParamsTable extends AbstractMigration
      */
     public function down(): string
     {
-        return '';
+        return 'DROP TABLE IF EXISTS `material_extra_params`;';
     }
 }
