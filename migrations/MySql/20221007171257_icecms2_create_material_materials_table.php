e8a1165d3e9006a74843a431cfd381453f5e2edd
Sergey Peshalov <desfpc@gmail.com>
2022-10-07 21:13:44 +0400

    Template migrations for CMS tables

---
+++ migrations/MySql/20221007171257_icecms2_create_material_materials_table.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+/**
+ * iceCMS2 v0.1a
+ * Created by Sergey Peshalov https://github.com/desfpc
+ * https://github.com/desfpc/iceCMS2
+ *
+ * Icecms2CreateMaterialMaterialsTable DB Migration
+ */
+
+namespace app\migrations\MySql;
+
+use iceCMS2\Cli\AbstractMigration;
+
+class Icecms2CreateMaterialMaterialsTable extends AbstractMigration
+{
+    /**
+     * Execute migration query
+     *
+     * @return string
+     */
+    public function up(): string
+    {
+        return '';
+    }
+
+    /**
+     * Rollback Migration
+     *
+     * @return string
+     */
+    public function down(): string
+    {
+        return '';
+    }
+}
