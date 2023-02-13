5389a7cd9ca9a2ea4f22e7eb59271369d5f8bb1e
Sergey Peshalov <desfpc@gmail.com>
2023-02-13 08:56:30 +0400

    Update main.php

--- templates/vendor/ice/Authorize/main.php
+++ templates/vendor/ice/Authorize/main.php
@@ -19,6 +19,46 @@ use iceCMS2\Controller\AbstractController;
             <?php include($this->_getLayoutPath() . '_alerts.php'); ?>
             <p>&nbsp;</p>
             <h1>Authorization</h1>
+            <div class="row d-flex justify-content-center">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-6">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+
+                            <div class="mb-md-5 mt-md-4 pb-5">
+
+                                <p class="text-white-50 mb-3">Please enter your email and password!</p>
+
+                                <div class="form-outline form-white mb-2">
+                                    <input type="email" id="typeEmailX" class="form-control form-control-lg">
+                                    <label class="form-label" for="typeEmailX" style="margin-left: 0px;">Email</label>
+                                    <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 40px;"></div><div class="form-notch-trailing"></div></div></div>
+
+                                <div class="form-outline form-white mb-2">
+                                    <input type="password" id="typePasswordX" class="form-control form-control-lg">
+                                    <label class="form-label" for="typePasswordX" style="margin-left: 0px;">Password</label>
+                                    <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 64.8px;"></div><div class="form-notch-trailing"></div></div></div>
+
+                                <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+                                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                                <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                                    <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                                    <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                    <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                                </div>
+
+                            </div>
+
+                            <div>
+                                <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a></p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p>&nbsp;</p>
         </div>
     </div>
 </div>
