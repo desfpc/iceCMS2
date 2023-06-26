6f23672092fc328efe7bb0b47e219a84d6b911bd
Sergey Peshalov <desfpc@gmail.com>
2023-06-27 02:19:50 +0400

    Update desfpc/vuebootstrap, load validation vue component

--- vendor/desfpc/icecms2/Web/js/authorize/profile-app.js
+++ vendor/desfpc/icecms2/Web/js/authorize/profile-app.js
@@ -1,6 +1,10 @@
 const { createApp, ref } = Vue
 import App from '/js/vendor/authorize/profile.js'
+import Validation from '/js/vuebootstrap/validation.js'
+import Validate from '/js/vuebootstrap/validate.js'
 
 const app = createApp(App)
 
+app.component("Validation", Validation);
+app.component("Validate", Validate);
 app.mount("#app");
