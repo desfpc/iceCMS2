d816d111a98f81bb1d2d59ccac70d6fe1135a0ac
Sergei Peshalov <desfpc@gmail.com>
2024-01-21 19:59:52 +0100

    Using Tab Vue component

--- vendor/desfpc/icecms2/Templates/ice/Authorize/profile.php
+++ vendor/desfpc/icecms2/Templates/ice/Authorize/profile.php
@@ -63,124 +63,111 @@ $user = $this->templateData['user'];
                     {{ alert.message }} <button type="button" class="btn-close float-end" aria-label="Close" @click="hideAlert()"></button>
                 </div>
 
-                <Tabs tabsjson='[{"name": "Test_Tab1", "id": "test_tab_1"},
-                {"name": "Test_Tab2", "id": "test_tab_2"}]' startactivetab="test_tab_1">
-                    <div>ass</div>
-                    <div>ass2</div>
-                </Tabs>
-
-                <ul class="nav nav-tabs mb-5">
-                    <li class="nav-item">
-                        <a class="tab-link nav-link" :class="getTabClass('tab_1')" href="#" @click="selectTab('tab_1')">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="tab-link nav-link" :class="getTabClass('tab_2')" href="#" @click="selectTab('tab_2'); loadSocialData();">Social connections</a>
-                    </li>
-                </ul>
-
-                <div v-if="activeTab === 'tab_1'">
-                    <div class="mb-3 row">
-                        <div class="col-sm-12">
-                            <span class="me-3">Status: <span :class="statusBadge">{{ user.status }}</span></span>
-                            <span class="me-3">Role: <span :class="roleBadge">{{ user.role }}</span></span>
-                            <span class="me-3">Rating: <span :class="ratingBadge">{{ user.rating }}</span></span>
-                            <span class="me-3">Email approved: <span :class="emailApprovedBadge"><?=
-                                    $user->get('email_approved') ? 'true' : 'false'; ?></span></span>
-                            <span class="me-3">Phone approved: <span :class="phoneApprovedBadge"><?=
-                                    $user->get('phone_approved') ? 'true' : 'false'; ?></span></span>
+                <Tabs tabsjson='[{"name": "Profile", "id": "tab_profile"},
+                {"name": "Social connections", "id": "tab_social"}]' startactivetab="tab_profile">
+                    <template v-slot:tab_profile>
+                        <div class="mb-3 row">
+                            <div class="col-sm-12">
+                                <span class="me-3">Status: <span :class="statusBadge">{{ user.status }}</span></span>
+                                <span class="me-3">Role: <span :class="roleBadge">{{ user.role }}</span></span>
+                                <span class="me-3">Rating: <span :class="ratingBadge">{{ user.rating }}</span></span>
+                                <span class="me-3">Email approved: <span :class="emailApprovedBadge"><?=
+                                        $user->get('email_approved') ? 'true' : 'false'; ?></span></span>
+                                <span class="me-3">Phone approved: <span :class="phoneApprovedBadge"><?=
+                                        $user->get('phone_approved') ? 'true' : 'false'; ?></span></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <div class="col-lg-3 text-center">
-                            <img :src="user.avatar" class="img-thumbnail user_avatar">
-                            <br><input type="file" class="form-control" id="file" ref="file" @change="onUploadFiles" />
-                        </div>
-                        <div class="col-sm-9">
-                            <div class="mb-3 row">
-                                <label for="name" class="col-sm-1 col-form-label text-end">Sex: </label>
-                                <div class="col-sm-5">
-                                    <Validate v-slot="{ errorClass }" :rule="$validationRules.string" :value="user.sex" :func="$globalValidation">
-                                        <select class="form-control" :class="errorClass" v-model="user.sex">
-                                            <option v-for="sex in sexes" :value="sex.value">{{ sex.text }}</option>
-                                        </select>
-                                    </Validate>
-                                </div>
+                        <div class="mb-3 row">
+                            <div class="col-lg-3 text-center">
+                                <img :src="user.avatar" class="img-thumbnail user_avatar">
+                                <br><input type="file" class="form-control" id="file" ref="file" @change="onUploadFiles" />
                             </div>
-                            <div class="mb-3 row">
-                                <label for="staticEmail" class="col-sm-1 col-form-label text-end">Email: </label>
-                                <div class="col-sm-5">
-                                    <input type="text" readonly disabled class="form-control" id="staticEmail" value="<?=
-                                    $user->get('email'); ?>">
-                                </div>
-                                <label for="name" class="col-sm-1 col-form-label text-end">Name: </label>
-                                <div class="col-sm-5">
-                                    <Validate v-slot="{ errorClass }" :rule="$validationRules.empty" :value="user.name" :func="$globalValidation">
-                                        <input type="text" class="form-control" :class="errorClass" id="name" v-model="user.name">
-                                    </Validate>
-                                </div>
-                            </div>
-                            <div class="mb-3 row">
-                                <label for="telegram" class="col-sm-1 col-form-label text-end">Telegram: </label>
-                                <div class="col-sm-5">
-                                    <Validate v-slot="{ errorClass }" :rule="$validationRules.empty" :value="user.telegram" :func="$globalValidation">
-                                        <input type="text" class="form-control" :class="errorClass" id="telegram" v-model="user.telegram">
-                                    </Validate>
-                                </div>
-                                <label for="nikname" class="col-sm-1 col-form-label text-end">Nickname: </label>
-                                <div class="col-sm-5">
-                                    <Validate v-slot="{ errorClass }" :rule="$validationRules.string" :value="user.nikname" :func="$globalValidation">
-                                        <input type="text" class="form-control" :class="errorClass" id="nikname" v-model="user.nikname">
-                                    </Validate>
-                                </div>
-                            </div>
-                            <div class="mb-3 row">
-                                <hr style="padding: 0 10px; background: #dddddd; width: 100%; height: 1px; border: 0;" />
-                                <div class="col-sm-6">
-                                    <div class="row">
-                                        <label for="oldPassword" class="col-sm-12 col-form-label">Old password: </label>
+                            <div class="col-sm-9">
+                                <div class="mb-3 row">
+                                    <label for="name" class="col-sm-1 col-form-label text-end">Sex: </label>
+                                    <div class="col-sm-5">
+                                        <Validate v-slot="{ errorClass }" :rule="$validationRules.string" :value="user.sex" :func="$globalValidation">
+                                            <select class="form-control" :class="errorClass" v-model="user.sex">
+                                                <option v-for="sex in sexes" :value="sex.value">{{ sex.text }}</option>
+                                            </select>
+                                        </Validate>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <input type="password" class="form-control" id="oldPassword" v-model="password.old">
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="staticEmail" class="col-sm-1 col-form-label text-end">Email: </label>
+                                    <div class="col-sm-5">
+                                        <input type="text" readonly disabled class="form-control" id="staticEmail" value="<?=
+                                        $user->get('email'); ?>">
+                                    </div>
+                                    <label for="name" class="col-sm-1 col-form-label text-end">Name: </label>
+                                    <div class="col-sm-5">
+                                        <Validate v-slot="{ errorClass }" :rule="$validationRules.empty" :value="user.name" :func="$globalValidation">
+                                            <input type="text" class="form-control" :class="errorClass" id="name" v-model="user.name">
+                                        </Validate>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="telegram" class="col-sm-1 col-form-label text-end">Telegram: </label>
+                                    <div class="col-sm-5">
+                                        <Validate v-slot="{ errorClass }" :rule="$validationRules.empty" :value="user.telegram" :func="$globalValidation">
+                                            <input type="text" class="form-control" :class="errorClass" id="telegram" v-model="user.telegram">
+                                        </Validate>
+                                    </div>
+                                    <label for="nikname" class="col-sm-1 col-form-label text-end">Nickname: </label>
+                                    <div class="col-sm-5">
+                                        <Validate v-slot="{ errorClass }" :rule="$validationRules.string" :value="user.nikname" :func="$globalValidation">
+                                            <input type="text" class="form-control" :class="errorClass" id="nikname" v-model="user.nikname">
+                                        </Validate>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <hr style="padding: 0 10px; background: #dddddd; width: 100%; height: 1px; border: 0;" />
+                                    <div class="col-sm-6">
+                                        <div class="row">
+                                            <label for="oldPassword" class="col-sm-12 col-form-label">Old password: </label>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input type="password" class="form-control" id="oldPassword" v-model="password.old">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="row">
+                                            <label for="newPassword" class="col-sm-12 col-form-label">New password: </label>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input type="password" class="form-control" id="newPasswordnn" v-model="password.new">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6">
-                                    <div class="row">
-                                        <label for="newPassword" class="col-sm-12 col-form-label">New password: </label>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-12">
-                                            <input type="password" class="form-control" id="newPasswordnn" v-model="password.new">
-                                        </div>
+                                <div class="mb-3 row">
+                                    <div class="col-sm">
+                                        <input type="button" class="btn btn-danger" :class="changePasswordStatus" @click="changePassword" value="Change password">
                                     </div>
                                 </div>
                             </div>
-                            <div class="mb-3 row">
-                                <div class="col-sm">
-                                    <input type="button" class="btn btn-danger" :class="changePasswordStatus" @click="changePassword" value="Change password">
-                                </div>
+                        </div>
+                        <div class="mb-3 pt-3 row">
+                            <div class="col-sm-12 col-form-label"><h5>Additional Contacts: </h5></div>
+                        </div>
+                        <div class="row mb-3" v-for="contact in contacts">
+                            <label class="col-sm-1 col-form-label">{{ contact }} </label>
+                            <div class="col-sm-11">
+                                <input type="text" class="form-control" v-model="user.contacts[contact]">
                             </div>
                         </div>
-                    </div>
-                    <div class="mb-3 pt-3 row">
-                        <div class="col-sm-12 col-form-label"><h5>Additional Contacts: </h5></div>
-                    </div>
-                    <div class="row mb-3" v-for="contact in contacts">
-                        <label class="col-sm-1 col-form-label">{{ contact }} </label>
-                        <div class="col-sm-11">
-                            <input type="text" class="form-control" v-model="user.contacts[contact]">
+                        <div class="mb-3 pt-3 row">
+                            <div class="col-sm-1">
+                                <button type="button" class="btn btn-primary" @click="save">Save</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3 pt-3 row">
-                        <div class="col-sm-1">
-                            <button type="button" class="btn btn-primary" @click="save">Save</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div v-if="activeTab === 'tab_2'">
-                    <!--<div class="accordion" id="accordionExample">
+                    </template>
+                    <template v-slot:tab_social>
+                        Hard working for this!
+                        <!--<div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header">
                                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -230,7 +217,8 @@ $user = $this->templateData['user'];
                             </div>
                         </div>
                     </div>-->
-                </div>
+                    </template>
+                </Tabs>
 
             </div>
 
