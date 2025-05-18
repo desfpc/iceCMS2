3b80e5855fe8ee34de50db48aa8a0933d56bbebc
Sergei Peshalov <desfpc@gmail.com>
2025-05-18 20:52:35 +0200

    Тестовые роуты для icecms

--- icecms/settings/routers.php
+++ icecms/settings/routers.php
@@ -2,10 +2,28 @@
 
 $routers = [
     '' => ['controller' => 'Tests', 'controllerMethod' => 'main', 'useVendor' => false],
-    'tests' => [ //Users list
+    'tests' => [
         'method' => 'GET',
         'controller' => 'Tests',
         'controllerMethod' => 'tests',
+        'useVendor' => false
+    ],
+    'test/$id/test/$subid/test/$subsubid' => [
+        'method' => 'GET',
+        'controller' => 'Tests',
+        'controllerMethod' => 'threetest',
+        'useVendor' => false
+    ],
+    'test/$id/test/$subid' => [
+        'method' => 'GET',
+        'controller' => 'Tests',
+        'controllerMethod' => 'twotest',
+        'useVendor' => false
+    ],
+    'test/$id' => [
+        'method' => 'GET',
+        'controller' => 'Tests',
+        'controllerMethod' => 'onetest',
         'useVendor' => false
     ],
 ];
